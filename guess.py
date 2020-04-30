abc16287b224f0fc3b1ac201d475f7359facdef7
axtrace <sofinma@ya.ru>
2020-04-30 23:20:59 +0300

    start using session_state and intents

--- guess.py
+++ guess.py
@@ -1,8 +1,17 @@
 import random
-import pickle
-import os
 
-file_path = os.path.normpath(r'/tmp/myfile.dat')
+
+def while_or_black(text):
+    whites = ('белый', 'белая', 'белые', 'белое', 'белого')
+    blacks = ('черный', 'черная', 'черное', 'черные', 'чёрн', 'черного', 'черн')
+    for c in whites:
+        if c in text:
+            return 'WHITE'
+    for c in blacks:
+        if c in text:
+            return 'BLACK'
+    return ''
+
 
 def square_color(square):
     file = square[0]
@@ -12,18 +21,6 @@ def square_color(square):
     if (rank + file_num) % 2 == 0:
         return 'BLACK'
     return 'WHITE'
-
-def while_or_black(text):
-    whites = ('белый', 'белая', 'белые', 'белое', 'белого')
-    blacks = ('черный', 'черная', 'черное', 'черные', 'чёрн', 'черного','черн')
-    for c in whites:
-        if c in text:
-            return 'WHITE'
-    for c in blacks:
-        if c in text:
-            return 'BLACK'
-    return ''
-
 
 def random_square():
     files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -51,27 +48,6 @@ def say_color(color):
         return 'чёрного'
     return ''
 
-def dump_to_file(user_id, square, color):      
-    with open(file_path, 'wb') as f:
-        if os.stat(file_path).st_size > 0:
-            data = pickle.load(f)
-        else:
-            data = dict()
-        data[user_id] = {'square':square, 'color': color}
-        pickle.dump(data, f)
-
-def load_from_file(user_id):
-    square = ''
-    color = ''
-    data = dict()
-    with open(file_path, 'rb') as f:
-        if os.stat(file_path).st_size > 0:
-            data = pickle.load(f)
-    user_data = data.get(user_id,'')
-    if user_data:
-        square = user_data.get('square', '')
-        color = user_data.get('color','')
-    return square,color
 
 
 def handler(event, context):
@@ -82,42 +58,41 @@ def handler(event, context):
     :return: response to be serialized as JSON.
     """
 
-    answer = 'Привет! Давай поиграем. Я буду называть клетку шахматной доски, а ты попробуй угадать её цвет. Для начала игры скажите "Да". Начинаем?'
+    answer = 'Привет! Давай поиграем. Я буду называть клетку шахматной доски, а ты попробуй угадать её цвет. Начинаем?'
     answer_tts = ''
     square = ''
-    color = ''
     is_session_end = 'false'
-    right_count = 0
 
     if event is not None and 'request' in event and 'original_utterance' in event['request'] and len(event['request']['original_utterance']):
         text = event['request']['command']
+        intents = event['request']['nlu']['intents']
 
-        user_color = while_or_black(text.lower())
-        user_id = event['session']['user_id']
-
-        if 'помощь' in text.lower() or 'что ты умеешь' in text.lower():
+        if 'YANDEX.HELP' in intents:
             answer = 'Я называю координаты шахматной клетки, а ты угадываешь её цвет. Например, а1 - чёрный, a2 - белый'
-        elif 'да' in text.lower():
+        elif 'YANDEX.CONFIRM' in intents or 'да' in  text:
             square = random_square()
             color = square_color(square)
             answer = f'Какого цвета клетка {square}?'
             answer_tts = f'Какого цвета клетка {say_square(square)}?'
-            dump_to_file(user_id, square, color)
-        elif user_color:
-            square, color = load_from_file(user_id)
+        elif 'WHITE_WORD' in intents or 'BLACK_WORD' in intents or while_or_black(text.lower()):
+            prev_square = event['state']['session']['square']
+            color = square_color(prev_square)
+            user_color = 'BLACK'
+            if 'WHITE_WORD' in intents or while_or_black(text.lower()) == 'WHITE':
+                user_color = 'WHITE'
             if user_color == color:
-                answer = 'Правильно!'
+                answer = 'Правильно! '
+                answer_tts = 'Правильно! '
             else:
-                answer = f'Неправильно! Клетка {square} {say_color(color)} цвета'
-                answer_tts = f'Неправильно! Клетка {say_square(square)} {say_color(color)} цвета'
+                answer = f'Неправильно! Клетка {prev_square} {say_color(color)} цвета. '
+                answer_tts = f'Неправильно! Клетка {say_square(prev_square)} {say_color(color)} цвета. '
             square = random_square()
-            color = square_color(square)
-            dump_to_file(user_id, square, color)
-            answer += f'. А теперь скажи, какого цвета клетка {square}?'
-            answer_tts += f'. А теперь скажи, какого цвета клетка {say_square(square)}?' 
+            answer += f'А теперь скажи, какого цвета клетка {square}?'
+            answer_tts += f'А теперь скажи, какого цвета клетка {say_square(square)}?' 
         else:
-             answer = f'Я не поняла. Назови цвет клетки {square}: белый или чёрный'
-             answer_tts = f'Я не поняла. Назови цвет клетки {say_square(square)}: белый или чёрный'
+            square = event['state']['session']['square']
+            answer = f'Я не поняла. Назови цвет клетки {square}: белый или чёрный'
+            answer_tts = f'Я не поняла. Назови цвет клетки {say_square(square)}: белый или чёрный'
     if not len(answer_tts):
         answer_tts = answer
     return {
@@ -128,4 +103,7 @@ def handler(event, context):
             'tts': answer_tts,
             'end_session': is_session_end
         },
+        'session_state': {
+            'square': square
+            }
     }
